aef7f30f33371151b5cb569366511c32c8fafc85
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2019-05-28 11:52:15 +0200

    Finished participations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Rails.application.routes.draw do
+  get 'participations/create'
+  get 'participations/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :events, only: %i[index show] do
+
+    resources :participations, only: %i[create destroy]
+
+    collection do
+      get 'search', to: 'events#search'
+    end
+
+  end
+
+  resources :participations, only: %i[destroy]
 end
