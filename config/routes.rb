ccc104f803a2ebb939dd24c243d3230a36887ae7
Victoria Reypin <reypinvictoria@gmail.com>
2019-05-28 13:31:19 +0200

    controller event

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'events/index'
+  get 'events/show'
+  get 'events/new'
+  get 'events/create'
+  get 'events/update'
+  get 'events/delete'
+  get 'events/validated'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
