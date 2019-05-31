d575ab55ffde498099085bd27f27bb9d2c1c2b7d
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2019-05-31 16:32:22 +0200

    Finished fixing search page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
   root to: 'pages#home'
   resources :users, only: :create
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
