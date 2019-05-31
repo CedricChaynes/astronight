a52bd3d57b49b20c4bb8e60b43e88f7ea2d1299e
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2019-05-31 10:38:33 +0200

    Finished scoring

--- app/services/calc_score.rb
+++ app/services/calc_score.rb
@@ -6,31 +6,38 @@ class CalcScore
   end
 
   def call
-    # TO DO algorithmie de calcul du score
+    # calculate score based on light pollution
     light_pol_score = @site.light_pol_index
 
+    # calculate score based on cloudness
     json_from_file = File.read("#{Rails.root}/app/services/weather_by_site_by_night.json")
     sites = JSON.parse(json_from_file)
-    site_data = sites.find { |site| @site.lat == site["Latitude"] && @site.lng == site["Longitude"] }
+    site_data = sites.find { |site| [@site.lat, @site.lng] == [site["Latitude"], site["Longitude"]] }
     weather_that_night = site_data["Weather_next_5_nights"]
                          .find { |weather| weather["night"] == @date.strftime('%d-%m-%Y') }
-    avg_cloudness = weather_that_night["weathers"].sum { |h| h["cloudness"] } / weather_that_night["weathers"].size
+    if weather_that_night["weathers"].empty?
+      cloudness_score = -100
+    else
+      avg_cloudness = weather_that_night["weathers"].sum { |h| h["cloudness"] } / weather_that_night["weathers"].size
+      cloudness_score = case avg_cloudness
+                        when 0...10 then 20
+                        when 10...20 then 15
+                        when 20...40 then 10
+                        when 40...60 then 5
+                        else -100
+                        end
+    end
 
-    cloudness_score = case avg_cloudness
-                      when 0...10 then 20
-                      when 10...20 then 15
-                      when 20...40 then 10
-                      when 40...60 then 5
-                      else -100
-                      end
-
+    # calculate score based on astro event significance
     astro_event_score = AstroEvent.where(date: @date).score
 
+    # calculate score based on moon status
     moon_score = ((1 - Lunartic.on_date(@date).percent_full) * 20).round(0)
 
-    score = (light_pol_score + cloudness_score + astro_event_score + moon_score) / 4
-
-    return score.positive ? score : 0
+    # calculate total score from 4 criteria
+    total_score = (light_pol_score + cloudness_score + astro_event_score + moon_score) / 4
+    total.score
+    return total_score.positive ? total_score : 0
   end
 
   def self.call(site, date)
