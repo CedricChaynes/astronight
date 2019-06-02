9ed83205a4106ad6b08c28726c4200ac40b473d0
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2019-06-03 00:09:50 +0200

    Added some unit testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,4 +7,16 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  # Devise test helpers
+  include Warden::Test::Helpers
+  Warden.test_mode!
 end
+
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    'chromeOptions': { args: %w[headless disable-gru window-size=1200,800] })
+  Capybara::Selenium::Driver.new app, browser: :chrome, desired_capabilities: capabilities
+end
+
+Capybara.save_path = Rails.root.join('tmp/capybara')
+Capybara.javascript_driver = :headless_chrome
