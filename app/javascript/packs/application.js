f52feb449ce8c166476c95b1051fd582e6f958f0
Adele Soullier <adele.soullier@gmail.com>
2019-06-03 16:59:53 +0200

    added gauge on search results

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,9 @@ import 'mapbox-gl/dist/mapbox-gl.css'
 import { placesAutocomplete } from './placesAutocomplete'
 import { initMapbox } from './initMapbox'
 import { launchLoader } from './launchLoader'
+import { initGauge } from './initGauge'
 
 placesAutocomplete()
 initMapbox()
 launchLoader()
+initGauge()
