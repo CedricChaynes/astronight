6d41e47b5dd7601b5c48ac7d5cf7e46becb1228f
Adele Soullier <adele.soullier@gmail.com>
2019-06-04 11:13:08 +0200

    created env variable for loader and gauge launch

--- app/javascript/packs/launchLoader.js
+++ app/javascript/packs/launchLoader.js
@@ -2,17 +2,22 @@ import { CountUp } from 'countup.js'
 
 const loader = document.querySelector(".loader");
 
-const launchLoader = () => {
+const launchLoader = (callback) => {
   if (loader) {
-  loader.style.display = "block";
-  const countUpSite = new CountUp('countup-site', 587, {duration: 4, separator: ' ', suffix: ' sites d\'observation'});
-  countUpSite.start()
-  const countUpLightPol = new CountUp('countup-light-pol', 934, {duration: 4, separator: ' ', suffix: ' indices de pollution lumineuse'});
-  countUpLightPol.start()
-  const countUpAstro = new CountUp('countup-astro-events', 114, {duration: 3, separator: ' ', suffix: ' événements astronomiques'});
-  countUpAstro.start()
-  const countUpMeteo = new CountUp('countup-meteo', 11740, {duration: 6, separator: ' ', suffix: ' bulletins météo nocturne'});
-  countUpMeteo.start(() => loader.classList.add("fade-out"))
+    // document.querySelector.('#searchbtn')
+    loader.style.display = "block";
+    const countUpSite = new CountUp('countup-site', 587, {duration: window.animationDuration + 1, separator: ' ', suffix: ' sites d\'observation'});
+    countUpSite.start()
+    const countUpLightPol = new CountUp('countup-light-pol', 934, {duration: window.animationDuration + 1, separator: ' ', suffix: ' indices de pollution lumineuse'});
+    countUpLightPol.start()
+    const countUpAstro = new CountUp('countup-astro-events', 114, {duration: window.animationDuration, separator: ' ', suffix: ' événements astronomiques'});
+    countUpAstro.start()
+    const countUpMeteo = new CountUp('countup-meteo', 11740, {duration: window.animationDuration + 2, separator: ' ', suffix: ' bulletins météo nocturne'});
+    countUpMeteo.start(() => {
+        loader.classList.add("fade-out")
+        callback()
+      }
+    )
   }
 };
 
