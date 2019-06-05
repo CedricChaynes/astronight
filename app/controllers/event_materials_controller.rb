1fadeda7044a34a812509f1ec07ee1e98ddb8494
CedricChaynes <cedric.chaynes@outlook.com>
2019-06-05 20:00:33 +0200

    event materials controller

--- app/controllers/event_materials_controller.rb
+++ app/controllers/event_materials_controller.rb
@@ -1,10 +1,10 @@
 class EventMaterialsController < ApplicationController
-  def create
-    EventMaterial.new(event_material_params)
-    EventMaterial.participation = @participation
-    EventMaterial.save
-    redirect_to event_path(@event)
-  end
+  # def create
+  #   EventMaterial.new(event_material_params)
+  #   EventMaterial.participation = @participation
+  #   EventMaterial.save
+  #   redirect_to event_path(@event)
+  # end
 
-  private
+  # private
 end
