3e1ac53904b74b4df8258252d6c634236c3596c5
Victoria Reypin <reypinvictoria@gmail.com>
2019-06-05 16:34:10 +0200

    meta

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,4 +21,8 @@ class ApplicationController < ActionController::Base
     Raven.user_context(id: session[:current_user_id]) # or anything else in session
     Raven.extra_context(params: params.to_unsafe_h, url: request.url)
   end
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "www.astronight.space" }
+  end
 end
