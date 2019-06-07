d1897a1b8c10bd669a8d700755e501aafc86d933
Kevin Chavanne <kevin.chavanne@gmail.com>
2019-06-07 10:07:36 +0200

    Center counters

--- app/javascript/packs/launchLoader.js
+++ app/javascript/packs/launchLoader.js
@@ -6,22 +6,22 @@ const loader = document.querySelector(".loader");
 const launchLoader = (callback) => {
   if (loader) {
     setTimeout(() => {
-      const countUpSite = new CountUp('countup-site', 587, {duration: window.animationDuration + 3, separator: ' ', suffix: ' sites d\'observation'});
+      const countUpSite = new CountUp('countup-site', 587, {duration: window.animationDuration + 3, separator: ' ', prefix: '<span class="countup-counter">', suffix: '</span> sites d\'observation'});
       countUpSite.start();
     }, 500);
 
     setTimeout(() => {
-      const countUpMeteo = new CountUp('countup-meteo', 11740, {duration: window.animationDuration + 6, separator: ' ', suffix: ' bulletins météo nocturne'});
+      const countUpMeteo = new CountUp('countup-meteo', 11740, {duration: window.animationDuration + 6, separator: ' ', prefix: '<span class="countup-counter">', suffix: '</span> bulletins météo nocturne'});
       countUpMeteo.start();
     }, 800)
 
     setTimeout(() => {
-      const countUpAstro = new CountUp('countup-astro-events', 114, {duration: window.animationDuration + 2, separator: ' ', suffix: ' événements astronomiques'});
+      const countUpAstro = new CountUp('countup-astro-events', 114, {duration: window.animationDuration + 2, separator: ' ', prefix: '<span class="countup-counter">', suffix: '</span> événements astronomiques'});
       countUpAstro.start();
     }, 1200)
 
     setTimeout(() => {
-      const countUpLightPol = new CountUp('countup-light-pol', 934, {duration: window.animationDuration + 4, separator: ' ', suffix: ' indices de pollution lumineuse'});
+      const countUpLightPol = new CountUp('countup-light-pol', 934, {duration: window.animationDuration + 4, separator: ' ', prefix: '<span class="countup-counter">', suffix: '</span> indices de pollution lumineuse'});
       countUpLightPol.start(() => {
         document.querySelector('body').classList.remove('has-loader');
         loader.classList.add("fade-out");
