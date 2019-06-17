219aaeaa5fba5d354e0a96bcd7b73fd6eae3a63f
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2019-06-17 13:27:27 +0200

    Finished cancel participation

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -1,39 +1,39 @@
 require 'test_helper'
 
 class EventsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get events_index_url
-    assert_response :success
-  end
+  # test "should get index" do
+  #   get events_index_url
+  #   assert_response :success
+  # end
 
-  test "should get show" do
-    get events_show_url
-    assert_response :success
-  end
+  # test "should get show" do
+  #   get events_show_url
+  #   assert_response :success
+  # end
 
-  test "should get new" do
-    get events_new_url
-    assert_response :success
-  end
+  # test "should get new" do
+  #   get events_new_url
+  #   assert_response :success
+  # end
 
-  test "should get create" do
-    get events_create_url
-    assert_response :success
-  end
+  # test "should get create" do
+  #   get events_create_url
+  #   assert_response :success
+  # end
 
-  test "should get update" do
-    get events_update_url
-    assert_response :success
-  end
+  # test "should get update" do
+  #   get events_update_url
+  #   assert_response :success
+  # end
 
-  test "should get delete" do
-    get events_delete_url
-    assert_response :success
-  end
+  # test "should get delete" do
+  #   get events_delete_url
+  #   assert_response :success
+  # end
 
-  test "should get validated" do
-    get events_validated_url
-    assert_response :success
-  end
+  # test "should get validated" do
+  #   get events_validated_url
+  #   assert_response :success
+  # end
 
 end
