274b563866c299d9643b2811761e8634dec887e8
Nhat Tai NGUYEN <nhattai.nguyen88@gmail.com>
2020-07-29 11:44:56 +0200

    Refacto calc_score service

--- app/services/calc_score.rb
+++ app/services/calc_score.rb
@@ -1,50 +1,16 @@
 # frozen_string_literal: true
 
-class CalcScore
+class CalcScore < BaseService
   require "lunartic"
+
+  attr_reader :site, :date
+
   def initialize(site, date)
     @site = site
     @date = date
   end
 
   def call
-    p @site
-    # calculate score based on light pollution
-    light_pol_score = @site.light_pol_index
-    p "Light polution score: #{light_pol_score}"
-    # calculate score based on cloudness
-    if @site.next_5_days_meteo.nil?
-      cloudiness_score = -100
-    else
-      weather_that_night = @site.next_5_days_meteo
-                                .find { |hash| hash["night"] == @date.strftime("%d-%m-%Y") }
-      if weather_that_night["weathers"].empty?
-        cloudiness_score = -100
-      else
-        avg_cloudiness =
-          weather_that_night["weathers"].sum { |h| h["cloudiness"] } / weather_that_night["weathers"].size
-        cloudiness_score = case avg_cloudiness
-                           when 0...10 then 20
-                           when 10...20 then 15
-                           when 20...40 then 10
-                           when 40...60 then 5
-                           else -100
-                           end
-      end
-    end
-    p "Cloudiness score: #{cloudiness_score}"
-    # calculate score based on astro event significance
-    astro_event = AstroEvent.find_by(date: @date)
-    astro_event_score = astro_event ? astro_event.score : 0
-    p "Astronomical event score #{astro_event_score}"
-    # calculate score based on moon status
-    moon_score = ((1 - Lunartic.on_date(@date).percent_full) * 20).floor
-
-    # calculate total score from 4 criteria
-    total_score = ((light_pol_score + cloudiness_score + astro_event_score + moon_score) / 8).floor
-    total_score = total_score.positive? ? total_score : 0
-    p "Total score #{total_score}/20"
-    p "-" * 100
     { total_score: total_score,
       light_pol_score: light_pol_score,
       cloudiness_score: cloudiness_score,
@@ -52,7 +18,46 @@ class CalcScore
       moon_score: moon_score }
   end
 
-  def self.call(site, date)
-    new(site, date).call
+  private
+
+  def light_pol_score
+    @light_pol_score ||= site.light_pol_index
+  end
+
+  def cloudiness_score
+    score = -100
+    if site.next_5_days_meteo
+      weather_that_night = site.next_5_days_meteo
+                               .find { |h| h["night"] == date.strftime("%d-%m-%Y") }
+      score = cal_cloudiness_score(weather_that_night) unless weather_that_night["weathers"].empty?
+    end
+    @cloudiness_score ||= score
+  end
+
+  def cal_cloudiness_score(weather)
+    avg_cloudiness =
+      weather["weathers"].sum { |h| h["cloudiness"] } / weather["weathers"].size
+
+    case avg_cloudiness
+    when 0...10 then 20
+    when 10...20 then 15
+    when 20...40 then 10
+    when 40...60 then 5
+    else -100
+    end
+  end
+
+  def astro_event_score
+    @astro_event_score ||= AstroEvent.find_by(date: date)&.score || 0
+  end
+
+  def moon_score
+    @moon_score ||= ((1 - Lunartic.on_date(date).percent_full) * 20).floor
+  end
+
+  def total_score
+    sum = light_pol_score + cloudiness_score + astro_event_score + moon_score
+    avg = (sum / 8).floor
+    @total_score ||= avg.positive? ? avg : 0
   end
 end
